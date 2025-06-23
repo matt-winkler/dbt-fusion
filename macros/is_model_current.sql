2f21b18e1d190750e1679e425a4491be19fc8ae7
matt-winkler <matt.winkler@dbtlabs.com>
2025-06-23 13:36:51 -0600

    cleanup after AI fun

--- macros/is_model_current.sql
+++ macros/is_model_current.sql
@@ -1,13 +1,16 @@
 {% macro is_model_current() %}
+   
+    {% set meta_config = this.get('config').get('meta') %}
+    {% do log(meta_config, info=true) %}
 
     {% if model.get('version') and model.get('version') == model.get('latest_version') %}
 
         {{return(true)}}
     
     {# why does this not work? #}
-    {% elif model.get('config').get('meta').get('deprecation_date')
-        and model.get('config').get('meta').get('deprecation_date') < '2025-08-01' %}
-        
+    {% elif meta_config.get('deprecation_date') 
+       and meta_config.get('deprecation_date') < '2025-08-01' %}
+
         {{return(false)}}
         
     {% else %}
