cdda743befc2f6a745a77c3ae00b609c39f0cfec
Matt Winkler <matt.winkler@fishtownanalytics.com>
2025-10-20 02:33:53 +0000

    fix deprecations

--- macros/is_model_expired.sql
+++ macros/is_model_expired.sql
@@ -4,10 +4,9 @@
     {% set model_version = model.get('version') %}
     {% set model_latest_version = model.get('latest_version') %}
 
-    {% if model_version and model_version < model_latest_version %}
+    {% if execute and model_version and model_version < model_latest_version %}
 
       {% if model_config_meta.get('deprecation_date') < modules.datetime.datetime.now().strftime('%Y-%m-%d') %}
-      {# if model_config_meta.get('deprecation_date') < '2025-08-01' #}
         
         {% do exceptions.raise_compiler_error('model ' ~ model.name ~ '_v' ~ model_version ~ ' is expired') %}
       
